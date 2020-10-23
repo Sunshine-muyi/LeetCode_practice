433ca61b82b590a9ae67fbb2fa1714d0113a667d
Sunshine-MY <Sunshine-MY@users.noreply.github.com>
2020-10-23 15:55:47 +0800

    LHY-laptop

--- addtion_of_two_number.py
+++ addtion_of_two_number.py
@@ -5,20 +5,35 @@
 # 来源：力扣（LeetCode）
 # 链接：https://leetcode-cn.com/problems/two-sum
 # 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
-def twoSum(nums, target):
-    n = len(nums)
-    if n:
-        for i in range(n - 1):
-            for j in range(n - 1, n):
-                if nums[i] <= target and nums[j] <= target:
-                    if nums[i] + nums[j] == target:
-                        return [i, j]
+from typing import List
+
+
+# 暴力求解 双循环
+def Two_Sum_Basic(Nums: List[int], Target: int) -> List[int]:
+    lens = len(Nums)
+    if lens:
+        for i in range(lens - 1):
+            for j in range(i, lens - 1):
+                if Nums[i] + Nums[j] == Target:
+                    return [i, j]
         print('无结果')
     else:
         print('数据为空')
 
 
+# 用哈希表存储数据。
+def Two_Sum_Hash(Nums: List[int], Target: int) -> List[int]:
+    lens = len(Nums)
+    hash_Table = {}
+    for i, num in enumerate(Nums):
+        if Target - num in hash_Table:
+            return [hash_Table[Target - num], i]
+        hash_Table[Nums[i]] = i
+    return []
+
+
 if __name__ == '__main__':
     nums = [2, 7, 11, 15]
     target = int(input('输入目标:'))
-    print(twoSum(nums, 9))
+    print(Two_Sum_Basic(nums, target))
+    print(Two_Sum_Hash(nums, target))
